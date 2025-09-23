d2fd8715b9c60cea84b51a900abe3c0e14f46cf3
lichaochao <1377283509@qq.com>
2025-09-23 10:49:17 +0800

    切换登录方式回调至Flutter

--- android/src/main/java/com/sean/rao/ali_auth/login/OneKeyLoginPublic.java
+++ android/src/main/java/com/sean/rao/ali_auth/login/OneKeyLoginPublic.java
@@ -30,7 +30,7 @@ import io.flutter.plugin.common.EventChannel;
 public class OneKeyLoginPublic extends LoginParams {
     private static final String TAG = OneKeyLoginPublic.class.getSimpleName();
 
-    public OneKeyLoginPublic(Activity activity, EventChannel.EventSink _eventSink, Object arguments){
+    public OneKeyLoginPublic(Activity activity, EventChannel.EventSink _eventSink, Object arguments) {
         mActivity = activity;
         mContext = activity.getBaseContext();
         eventSink = _eventSink;
@@ -52,7 +52,7 @@ public class OneKeyLoginPublic extends LoginParams {
      * 初始化SDK
      */
     private void sdkInit() {
-        mTokenResultListener=new TokenResultListener() {
+        mTokenResultListener = new TokenResultListener() {
             @Override
             public void onTokenSuccess(String s) {
                 sdkAvailable = true;
@@ -83,9 +83,12 @@ public class OneKeyLoginPublic extends LoginParams {
                 Log.e(TAG, "获取token失败：" + s);
                 try {
                     TokenRet tokenRet = TokenRet.fromJson(s);
-                    List<String> skip = Collections.singletonList(ResultCode.CODE_ERROR_USER_SWITCH);
+                    List<String> skip = Collections.singletonList();
+                    Log.e(TAG, "获取token失败：skips = " + skip.toString() + " code = " + tokenRet.getCode() + " result =  "
+                            + tokenRet.getMsg());
                     if (!skip.contains(tokenRet.getCode())) {
-                        showResult(tokenRet.getCode(), tokenRet.getMsg(),null);
+                        Log.e(TAG, "获取token失败 CODE_ERROR_USER_SWITCH");
+                        showResult(tokenRet.getCode(), tokenRet.getMsg(), null);
                     }
                 } catch (Exception e) {
                     e.fillInStackTrace();
@@ -93,7 +96,7 @@ public class OneKeyLoginPublic extends LoginParams {
                 mAuthHelper.setAuthListener(null);
             }
         };
-        mAuthHelper=PhoneNumberAuthHelper.getInstance(mContext, mTokenResultListener);
+        mAuthHelper = PhoneNumberAuthHelper.getInstance(mContext, mTokenResultListener);
         mAuthHelper.getReporter().setLoggerEnable(jsonObject.getBooleanValue("isDebug"));
         mAuthHelper.setAuthSDKInfo(jsonObject.getString("androidSk"));
 
@@ -120,23 +123,25 @@ public class OneKeyLoginPublic extends LoginParams {
 
     /**
      * 延时登录操作
+     * 
      * @param timeout
      */
-    public void startLogin(int timeout){
+    public void startLogin(int timeout) {
         if (sdkAvailable) {
             mAuthHelper.quitLoginPage();
             getLoginToken(timeout);
         } else {
-            //如果环境检查失败 使用其他登录方式
+            // 如果环境检查失败 使用其他登录方式
         }
     }
 
     /**
      * 返回默认上网卡运营商
+     * 
      * @param
      * @return CMCC(移动)、CUCC(联通)、CTCC(电信)
      */
-    public String getCurrentCarrierName(){
+    public String getCurrentCarrierName() {
         return mAuthHelper.getCurrentCarrierName();
     }
 
@@ -152,6 +157,7 @@ public class OneKeyLoginPublic extends LoginParams {
      * 在不是一进app就需要登录的场景 建议调用此接口 加速拉起一键登录页面
      * 等到用户点击登录的时候 授权页可以秒拉
      * 预取号的成功与否不影响一键登录功能，所以不需要等待预取号的返回。
+     * 
      * @param timeout
      */
     private void accelerateLoginPage(int timeout) {
@@ -161,6 +167,7 @@ public class OneKeyLoginPublic extends LoginParams {
                 Log.e(TAG, "预取号成功: " + s);
                 showResult("600016", null, s);
             }
+
             @Override
             public void onTokenFailed(String s, String s1) {
                 Log.e(TAG, "预取号失败：" + ", " + s1);
@@ -174,6 +181,7 @@ public class OneKeyLoginPublic extends LoginParams {
 
     /**
      * 拉起授权页
+     * 
      * @param timeout 超时时间
      */
     public void getLoginToken(int timeout) {
@@ -186,7 +194,7 @@ public class OneKeyLoginPublic extends LoginParams {
                     if (ResultCode.CODE_START_AUTHPAGE_SUCCESS.equals(tokenRet.getCode())) {
                         Log.i(TAG, "唤起授权页成功：" + s);
                     }
-                    showResult(tokenRet.getCode(), tokenRet.getMsg(),tokenRet.getToken());
+                    showResult(tokenRet.getCode(), tokenRet.getMsg(), tokenRet.getToken());
                     if (ResultCode.CODE_SUCCESS.equals(tokenRet.getCode())) {
                         Log.i(TAG, "获取token成功：" + s);
                         mAuthHelper.setAuthListener(null);
@@ -199,10 +207,10 @@ public class OneKeyLoginPublic extends LoginParams {
             @Override
             public void onTokenFailed(String s) {
                 Log.e(TAG, "获取token失败：" + s);
-                //如果环境检查失败 使用其他登录方式
+                // 如果环境检查失败 使用其他登录方式
                 try {
                     TokenRet tokenRet = TokenRet.fromJson(s);
-                    showResult(tokenRet.getCode(), tokenRet.getMsg(),null);
+                    showResult(tokenRet.getCode(), tokenRet.getMsg(), null);
                 } catch (Exception e) {
                     e.fillInStackTrace();
                 }
@@ -214,45 +222,44 @@ public class OneKeyLoginPublic extends LoginParams {
         mAuthHelper.getLoginToken(mContext, timeout);
     }
 
-
     /**
      * SDK环境检查函数，检查终端是否⽀持号码认证，通过TokenResultListener返回code
      * type 1：本机号码校验 2: ⼀键登录
      * 600024 终端⽀持认证
      * 600013 系统维护，功能不可⽤
      */
-    public void checkEnvAvailable(@IntRange(from = 1, to = 2) int type){
+    public void checkEnvAvailable(@IntRange(from = 1, to = 2) int type) {
         mAuthHelper.checkEnvAvailable(type);
     }
-
 
     /**
      * 获取授权页协议勾选框选中状态
      */
-    public boolean queryCheckBoxIsChecked(){
+    public boolean queryCheckBoxIsChecked() {
         return mAuthHelper.queryCheckBoxIsChecked();
     }
 
     /**
      * 退出授权页面
      */
-    public void quitPage(){
+    public void quitPage() {
         mAuthHelper.quitLoginPage();
     }
 
     /**
      * 结束授权页loading dialog
      */
-    public void hideLoading(){
+    public void hideLoading() {
         mAuthHelper.hideLoginLoading();
     }
 
     /**
      * 处理参数，对参数进行处理包含color、Path
+     * 
      * @param parmas
      * @return
      */
-    private JSONObject formatParmas(Object parmas){
+    private JSONObject formatParmas(Object parmas) {
         JSONObject formatData = JSONObject.parseObject(JSONObject.toJSONString(parmas));
         for (Map.Entry<String, Object> entry : formatData.entrySet()) {
             // System.out.println(entry.getKey() + "----" + entry.getValue());
@@ -263,12 +270,10 @@ public class OneKeyLoginPublic extends LoginParams {
             }
             // 判断是否时路径字段
             // 排除按钮状态的背景logBtnBackgroundPath
-            else if (
-                    !entry.getKey().contains("logBtnBackgroundPath") &&
+            else if (!entry.getKey().contains("logBtnBackgroundPath") &&
                     entry.getKey().toLowerCase().contains("path") &&
                     !formatData.getString(entry.getKey()).isEmpty() &&
-                    !formatData.getString(entry.getKey()).contains("http")
-            ) {
+                    !formatData.getString(entry.getKey()).contains("http")) {
                 formatData.put(entry.getKey(), UtilTool.flutterToPath(formatData.getString(entry.getKey())));
             } else {
                 System.out.println(entry.getKey() + "--------------" + entry.getValue());
@@ -279,65 +284,71 @@ public class OneKeyLoginPublic extends LoginParams {
         return formatData;
     }
 
-
     /**
      * 对配置参数进行格式化并且转换
+     * 
      * @param jsonObject
      * @return
      */
-    private AuthUIConfig.Builder getFormatConfig(JSONObject jsonObject){
+    private AuthUIConfig.Builder getFormatConfig(JSONObject jsonObject) {
         AuthUIConfig.Builder config = JSON.parseObject(JSONObject.toJSONString(jsonObject), AuthUIConfig.Builder.class);
 
         // 设置按钮的背景
         // 20230518 修正错误 setLoadingBackgroundPath -> setLogBtnBackgroundPath
-        if (jsonObject.getString("logBtnBackgroundPath") != null && jsonObject.getString("logBtnBackgroundPath").contains(",")) {
-            config.setLogBtnBackgroundDrawable(UtilTool.getStateListDrawable(mContext, jsonObject.getString("logBtnBackgroundPath")));
+        if (jsonObject.getString("logBtnBackgroundPath") != null
+                && jsonObject.getString("logBtnBackgroundPath").contains(",")) {
+            config.setLogBtnBackgroundDrawable(
+                    UtilTool.getStateListDrawable(mContext, jsonObject.getString("logBtnBackgroundPath")));
         } else {
             config.setLogBtnBackgroundPath(UtilTool.flutterToPath(jsonObject.getString("logBtnBackgroundPath")));
         }
         /**
-         *  authPageActIn = var1;
-         *  activityOut = var2;
+         * authPageActIn = var1;
+         * activityOut = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "authPageActIn") && UtilTool.dataStatus(jsonObject, "activityOut")){
+        if (UtilTool.dataStatus(jsonObject, "authPageActIn") && UtilTool.dataStatus(jsonObject, "activityOut")) {
             config.setAuthPageActIn(jsonObject.getString("authPageActIn"), jsonObject.getString("activityOut"));
         }
         /**
-         *  authPageActOut = var1;
-         *  activityIn = var2;
+         * authPageActOut = var1;
+         * activityIn = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "authPageActOut") && UtilTool.dataStatus(jsonObject, "activityIn")){
+        if (UtilTool.dataStatus(jsonObject, "authPageActOut") && UtilTool.dataStatus(jsonObject, "activityIn")) {
             config.setAuthPageActOut(jsonObject.getString("authPageActOut"), jsonObject.getString("activityIn"));
         }
         /**
-         *  protocolOneName = var1;
-         *  protocolOneURL = var2;
+         * protocolOneName = var1;
+         * protocolOneURL = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "protocolOneName") && UtilTool.dataStatus(jsonObject, "protocolOneURL")){
+        if (UtilTool.dataStatus(jsonObject, "protocolOneName") && UtilTool.dataStatus(jsonObject, "protocolOneURL")) {
             config.setAppPrivacyOne(jsonObject.getString("protocolOneName"), jsonObject.getString("protocolOneURL"));
         }
         /**
-         *  protocolTwoName = var1;
-         *  protocolTwoURL = var2;
+         * protocolTwoName = var1;
+         * protocolTwoURL = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "protocolTwoName") && UtilTool.dataStatus(jsonObject, "protocolTwoURL")){
+        if (UtilTool.dataStatus(jsonObject, "protocolTwoName") && UtilTool.dataStatus(jsonObject, "protocolTwoURL")) {
             config.setAppPrivacyTwo(jsonObject.getString("protocolTwoName"), jsonObject.getString("protocolTwoURL"));
         }
         /**
-         *  protocolThreeName = var1;
-         *  protocolThreeURL = var2;
+         * protocolThreeName = var1;
+         * protocolThreeURL = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "protocolThreeName") && UtilTool.dataStatus(jsonObject, "protocolThreeURL")){
-            config.setAppPrivacyThree(jsonObject.getString("protocolThreeName"), jsonObject.getString("protocolThreeURL"));
+        if (UtilTool.dataStatus(jsonObject, "protocolThreeName")
+                && UtilTool.dataStatus(jsonObject, "protocolThreeURL")) {
+            config.setAppPrivacyThree(jsonObject.getString("protocolThreeName"),
+                    jsonObject.getString("protocolThreeURL"));
         }
         /**
-         *  protocolColor = var1;
-         *  protocolOneColor = var2;
-         *  protocolTwoColor = var2;
-         *  protocolThreeColor = var2;
+         * protocolColor = var1;
+         * protocolOneColor = var2;
+         * protocolTwoColor = var2;
+         * protocolThreeColor = var2;
          */
-        if(UtilTool.dataStatus(jsonObject, "protocolColor") && UtilTool.dataStatus(jsonObject, "protocolCustomColor")){
-            config.setAppPrivacyColor(jsonObject.getIntValue("protocolColor"), jsonObject.getIntValue("protocolCustomColor"));
+        if (UtilTool.dataStatus(jsonObject, "protocolColor")
+                && UtilTool.dataStatus(jsonObject, "protocolCustomColor")) {
+            config.setAppPrivacyColor(jsonObject.getIntValue("protocolColor"),
+                    jsonObject.getIntValue("protocolCustomColor"));
         }
         return config;
     }
